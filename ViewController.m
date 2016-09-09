91c46d32d48ae0459698e91e963e61f82c50e6a2
QL-Socket <qiulinggongzuo@163.com>
2016-09-09 23:35:14 +0800

    白色
    改为白色

--- ViewController.m
+++ ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor blueColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
