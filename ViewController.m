7cd72232a6dcb0846bc3934394ae7be551adceff
QL-Socket <qiulinggongzuo@163.com>
2016-09-09 23:54:06 +0800

    Revert "白色"
    This reverts commit 91c46d32d48ae0459698e91e963e61f82c50e6a2.

--- ViewController.m
+++ ViewController.m
@@ -16,9 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
-    
-    
+    self.view.backgroundColor = [UIColor blueColor];
 }
 
 - (void)didReceiveMemoryWarning {
